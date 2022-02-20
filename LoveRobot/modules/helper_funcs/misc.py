08c604020b4dfe9898200d0a579bdf34c109cae9
MichaelFaraday <88786146+Sumans11@users.noreply.github.com>
2022-02-20 05:48:07 +0530

    Update misc.py

--- LoveRobot/modules/helper_funcs/misc.py
+++ LoveRobot/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("☜", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="zaid_back"),
-             EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("👈", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Back", callback_data="love_back"),
+             EqInlineKeyboardButton("👉", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="zaid_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="love_back")]]
 
     return pairs
 
